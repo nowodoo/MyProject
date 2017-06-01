ec7d0edacf2a451479ccd0053ad5ea5e2f71bb15
maluyong <maluyong@tuniu.com>
2017-06-01 21:04:05 +0800

    LOG-3 jdkProxy add jdkProxy basic test.

--- common/src/test/java/com/zach/jdkproxy/JDKProxyTest.java
+++ common/src/test/java/com/zach/jdkproxy/JDKProxyTest.java
@@ -12,28 +12,21 @@ public class JDKProxyTest {
 
 
     @Test
-    public void test() {
-        System.out.println("run test now !");
-
-    }
-
-
-    public static void main(String[]args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    public void test() throws Exception{
         //这里有两种写法，我们采用略微复杂的一种写法，这样更有助于大家理解。
-        Class<?> proxyClass= Proxy.getProxyClass(JDKProxyTest.class.getClassLoader(),HelloWorld.class);
-        final Constructor<?> cons = proxyClass.getConstructor(InvocationHandler.class);
-        final InvocationHandler ih = new MyInvocationHandler(new HelloworldImpl());
-        HelloWorld helloWorld= (HelloWorld)cons.newInstance(ih);
-        helloWorld.sayHello();
+//        Class<?> proxyClass= Proxy.getProxyClass(JDKProxyTest.class.getClassLoader(),HelloWorld.class);
+//        final Constructor<?> cons = proxyClass.getConstructor(InvocationHandler.class);
+//        final InvocationHandler ih = new MyInvocationHandler(new HelloworldImpl());
+//        HelloWorld helloWorld= (HelloWorld)cons.newInstance(ih);
+//        helloWorld.sayHello();
 
         //下面是更简单的一种写法，本质上和上面是一样的
-        /* 
         HelloWorld helloWorld=(HelloWorld)Proxy.
                  newProxyInstance(JDKProxyTest.class.getClassLoader(),
                         new Class<?>[]{HelloWorld.class},
                         new MyInvocationHandler(new HelloworldImpl()));
         helloWorld.sayHello();
-        */
-    }
 
+
+    }
 }
