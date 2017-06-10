ce8581d25aec333f6b93248862c14586ff5fa08a
maluyong <maluyong@tuniu.com>
2017-06-10 14:31:21 +0800

    LOG-3 Disruptor生产者进行事件的发布。

--- disruptor/src/main/java/com/zach/common/LongEventProducer.java
+++ disruptor/src/main/java/com/zach/common/LongEventProducer.java
@@ -7,27 +7,27 @@ import java.nio.ByteBuffer;
 /**
  * 生产者类,数据来自一个ByteBuffer
  */
-public class LongEventProducer
-{
+public class LongEventProducer {
     private final RingBuffer<LongEvent> ringBuffer;
 
-    public LongEventProducer(RingBuffer<LongEvent> ringBuffer)
-    {
+    public LongEventProducer(RingBuffer<LongEvent> ringBuffer) {
         this.ringBuffer = ringBuffer;
     }
 
-    public void onData(ByteBuffer bb)
-    {
-        long sequence = ringBuffer.next();  // Grab the next sequence
-        try
-        {
-            LongEvent event = ringBuffer.get(sequence); // Get the entry in the Disruptor
-                                                        // for the sequence
-            event.set(bb.getLong(0));  // Fill with data
-        }
-        finally
-        {
-            ringBuffer.publish(sequence);
+    /**
+     * 首先使用ringBuffer获取一个接受参数的序号（sequence），然后封装好了一个longEvent事件,事件的发布是用ringBuffer的
+     * publish方法，实际在操作的是ringBuffer，数据的封装是用的LongEvent（数据槽）
+     *
+     * @param bb
+     */
+    public void onData(ByteBuffer bb) {
+        long sequence = ringBuffer.next();  //获取下一个序号
+
+        try {
+            LongEvent event = ringBuffer.get(sequence); // 在disruptor中获取数据的入口
+            event.set(bb.getLong(0));  //进行数据填充
+        } finally {
+            ringBuffer.publish(sequence);  //发布事件，通知消费者
         }
     }
 }
