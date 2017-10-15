55f82deb4f0a675c207ea01c2d63c55ae79c6369
maluyong <maluyong@tuniu.com>
2017-10-15 17:05:01 +0800

    LOG-3 策略模式添加注解。

--- designpattern/src/main/java/cn/javass/dp/strategy/example4/Client2.java
+++ designpattern/src/main/java/cn/javass/dp/strategy/example4/Client2.java
@@ -2,6 +2,9 @@ package cn.javass.dp.strategy.example4;
 
 public class Client2 {
 	public static void main(String[] args) {
+
+		//在这里可以进行判断。
+
 		//1：选择并创建需要使用的策略对象
 		Strategy strategy = new CooperateCustomerStrategy();
 		//2：创建上下文
