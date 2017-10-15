abacb15e372720423df1dad28f39e24b8a1ca81b
maluyong <maluyong@tuniu.com>
2017-10-15 16:31:30 +0800

    LOG-3 装饰器模式添加注解。

--- designpattern/src/main/java/cn/javass/dp/decorator/example6/GoodsSaleEbo.java
+++ designpattern/src/main/java/cn/javass/dp/decorator/example6/GoodsSaleEbo.java
@@ -3,6 +3,9 @@ package cn.javass.dp.decorator.example6;
 public class GoodsSaleEbo implements GoodsSaleEbi{
 	
 	public boolean sale(String user,String customer, SaleModel saleModel) {
+
+
+		//这里原有的方法。   以后要是需要装饰这个方法的话，就直接将这个方法提炼到接口中去就好了。
 		System.out.println(user+"保存了"+customer+"购买 "+saleModel+" 的销售数据");
 		return true;
 	}
