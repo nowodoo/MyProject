a0763d47f4b639d4bde3520bcec5e2a519137c05
maluyong <maluyong@tuniu.com>
2017-10-15 16:43:46 +0800

    LOG-3 装饰器模式添加注解。

--- designpattern/src/main/java/cn/javass/dp/decorator/example6/Client.java
+++ designpattern/src/main/java/cn/javass/dp/decorator/example6/Client.java
@@ -3,6 +3,7 @@ package cn.javass.dp.decorator.example6;
 public class Client {
 	public static void main(String[] args) {
 		//得到业务接口,组合装饰器
+		//这个对象在这里有一种递归调用。
 		GoodsSaleEbi ebi = new LogDecorator(new CheckDecorator(new GoodsSaleEbo()));
 		//准备测试数据
 		SaleModel saleModel = new SaleModel();
