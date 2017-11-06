6439a5bfbbc9c76095dda30012e8f0fac48ebf4d
maluyong <maluyong@tuniu.com>
2017-11-06 09:52:54 +0800

    LOG-3 工厂模式。

--- designpattern/src/main/java/cn/javass/dp/factorymethod/example7/ExportDB.java
+++ designpattern/src/main/java/cn/javass/dp/factorymethod/example7/ExportDB.java
@@ -5,7 +5,7 @@ package cn.javass.dp.factorymethod.example7;
 public class ExportDB implements ExportFileApi{
 	public boolean export(String data) {
 		//简单示意一下，这里需要操作数据库和文件
-		System.out.println("导出数据"+data+"到数据库备份文件");
+		System.out.println("导出数据" + data + "到数据库备份文件");
 		return true;
 	}
 }
