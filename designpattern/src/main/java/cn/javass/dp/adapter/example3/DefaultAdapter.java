e88b315ebebddde16ada1b86565ed1d64edc639e
maluyong <maluyong@tuniu.com>
2017-12-22 23:00:05 +0800

    LOG-3 关于虚实结合。

--- designpattern/src/main/java/cn/javass/dp/adapter/example3/DefaultAdapter.java
+++ designpattern/src/main/java/cn/javass/dp/adapter/example3/DefaultAdapter.java
@@ -4,22 +4,18 @@ import java.util.List;
 
 public class DefaultAdapter implements LogDbOperateApi{
 
-	@Override
 	public void createLog(LogModel lm) {
 		System.out.println("DB createLog===========");
 	}
 
-	@Override
 	public void updateLog(LogModel lm) {
 		System.out.println("DB updateLog===========");
 	}
 
-	@Override
 	public void removeLog(LogModel lm) {
 		
 	}
 
-	@Override
 	public List<LogModel> getAllLog() {
 		return null;
 	}
