c6ad006957597830366937a5708f697d1098c800
maluyong <maluyong@dianwoba.com>
2018-06-07 19:36:34 +0800

    LOG-0 cglib.

--- bytecode/src/main/java/com/zach/bytecode/cglib/proxy/CglibObjectProxy.java
+++ bytecode/src/main/java/com/zach/bytecode/cglib/proxy/CglibObjectProxy.java
@@ -10,6 +10,8 @@ public class CglibObjectProxy {
 
     @SuppressWarnings("rawtypes")
     public static Object ceateProxtObject(final Object object, Class clazz) {
+
+
         // 声明增加类实例  
         Enhancer en = new Enhancer();
         // 设置被代理类字节码，CGLIB根据字节码生成被代理类的子类  
@@ -19,14 +21,16 @@ public class CglibObjectProxy {
 
             @Override
             public Object intercept(Object arg0, Method method, Object[] args, MethodProxy arg3) throws Throwable {
-
                 System.out.println("proxy before!");
 
+
+                // 中间利用反射调用实际的方法,这个方法可以取到方法的参数.
                 // 注意参数object,仍然为外部声明的源对象，且Method为JDK的Method反射  
                 Object o = method.invoke(object, args);
 
-                System.out.println("proxy after!");
 
+
+                System.out.println("proxy after!");
                 return o;
             }
         });
