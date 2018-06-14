458d6acd01502d804e1e7d204107522017e48c66
maluyong <maluyong@dianwoba.com>
2018-06-14 17:37:52 +0800

    LOG-000 banner & spring mvc.

---
+++ dubbo/src/main/java/com/zach/dubbo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.zach.dubbo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/TestController")
+public class TestController {
+
+    /**
+     * curl http://127.0.0.1:8088/TestController/test
+     * @return
+     */
+    @RequestMapping("/test")
+    public String test() {
+        return "123";
+    }
+}
