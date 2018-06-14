b8cfae56504ce1b89c6233eae3c92de541f74ac3
maluyong <maluyong@dianwoba.com>
2018-06-14 16:03:04 +0800

    LOG-000 add dubbo service.

---
+++ dubbo/src/main/java/com/zach/dubbo/App.java
@@ -0,0 +1,20 @@
+package com.zach.dubbo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(App.class).profiles("dwd-dev").build(args).run(args);
+        logger.info("dubbo-service started !!!!");
+    }
+}
