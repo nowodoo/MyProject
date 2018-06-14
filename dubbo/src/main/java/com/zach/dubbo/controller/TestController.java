2a982e976ec97f510959424245adfbff5f7f3b9b
maluyong <maluyong@dianwoba.com>
2018-06-14 17:45:49 +0800

    LOG-000 for spring mvc, the reason why server.port doesn't work at the first time is that it's overrode by jvm parameters configured by idea at start command.

--- dubbo/src/main/java/com/zach/dubbo/controller/TestController.java
+++ dubbo/src/main/java/com/zach/dubbo/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.zach.dubbo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
