b69ae4115c3d5b968233e50eb4839ce1c16d942a
maluyong <maluyong@dianwoba.com>
2018-06-19 09:56:52 +0800

    LOG-000 begin using dubbo bean.

--- dubbo/src/main/java/com/zach/dubbo/controller/TestController.java
+++ dubbo/src/main/java/com/zach/dubbo/controller/TestController.java
@@ -1,11 +1,20 @@
 package com.zach.dubbo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+        import com.dianwoba.optimus.coupon.domain.result.RiderCouponResultDTO;
+        import com.dianwoba.optimus.coupon.param.RiderCouponQueryParam;
+        import com.dianwoba.optimus.coupon.provider.rider.RiderCouponQueryProvider;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
+
+        import javax.annotation.Resource;
+        import java.util.List;
 
 @RestController
 @RequestMapping("/TestController")
 public class TestController {
+
+    @Resource
+    RiderCouponQueryProvider riderCouponQueryProvider;
 
     /**
      * curl http://127.0.0.1:8088/TestController/test
@@ -13,6 +22,10 @@ public class TestController {
      */
     @RequestMapping("/test")
     public String test() {
+        RiderCouponQueryParam riderCouponQueryParam = new RiderCouponQueryParam();
+        riderCouponQueryParam.setUserId(2);
+
+        List<RiderCouponResultDTO> riderCouponResultDTOS = riderCouponQueryProvider.queryCoupon(riderCouponQueryParam);
         return "Hello Zach!";
     }
 }
