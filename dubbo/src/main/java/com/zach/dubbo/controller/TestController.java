d5c0675b9a6a09780ce679fc136f82a26d323ad9
maluyong <maluyong@dianwoba.com>
2018-06-20 14:40:39 +0800

    LOG-000 dubbo consumer can start.

--- dubbo/src/main/java/com/zach/dubbo/controller/TestController.java
+++ dubbo/src/main/java/com/zach/dubbo/controller/TestController.java
@@ -1,11 +1,20 @@
 package com.zach.dubbo.controller;
 
+import com.dianwoba.optimus.coupon.param.ValidationParam;
+import com.dianwoba.optimus.coupon.provider.user.CouponUseProvider;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/TestController")
 public class TestController {
+
+
+    @Resource
+    CouponUseProvider couponUseProvider;
+
 
     /**
      * curl http://127.0.0.1:8088/TestController/test
@@ -13,6 +22,22 @@ public class TestController {
      */
     @RequestMapping("/test")
     public String test() {
+        return "Hello Zach!";
+    }
+
+
+    /**
+     * curl http://127.0.0.1:8088/TestController/test
+     * @return
+     */
+    @RequestMapping("/validation")
+    public String validation() {
+
+        ValidationParam validationParam = new ValidationParam();
+        validationParam.setUserId(0L);
+
+
+        couponUseProvider.paramTest(validationParam);
         return "Hello Zach!";
     }
 }
