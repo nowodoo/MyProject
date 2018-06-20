5338bc592be10453e0fcff73328e582f46ae3e17
maluyong <maluyong@dianwoba.com>
2018-06-20 20:45:49 +0800

    log-000 solve tomcat abort suddenly.

--- dubboprovider/dubboproviderserver/src/main/java/com/zach/dubboprovider/api/impl/ValidateProviderImpl.java
+++ dubboprovider/dubboproviderserver/src/main/java/com/zach/dubboprovider/api/impl/ValidateProviderImpl.java
@@ -1,18 +1,19 @@
 package com.zach.dubboprovider.api.impl;
 
-//import com.zach.dubboprovider.api.ValidateProvider;
-//import com.zach.dubboprovider.dto.ValidateParam;
+import com.zach.dubboprovider.api.ValidateProvider;
+import com.zach.dubboprovider.dto.ValidateParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
-//public class ValidateProviderImpl implements ValidateProvider {
-public class ValidateProviderImpl {
+@Component
+public class ValidateProviderImpl implements ValidateProvider {
     private static Logger logger = LoggerFactory.getLogger(ValidateProviderImpl.class);
 
-//    @Override
-//    public boolean paramValidate(ValidateParam validateParam) {
-//        logger.info("hello man !");
-//
-//        return false;
-//    }
+    @Override
+    public boolean paramValidate(ValidateParam validateParam) {
+        logger.info("hello man !");
+
+        return false;
+    }
 }
