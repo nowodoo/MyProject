24532fdc4cecf7a88085b1f673cb11e862e11134
maluyong <maluyong@dianwoba.com>
2018-06-20 20:12:01 +0800

    log-000 dubbo-provider can start.

--- dubboprovider/dubboproviderserver/src/main/java/com/zach/dubboprovider/api/impl/ValidateProviderImpl.java
+++ dubboprovider/dubboproviderserver/src/main/java/com/zach/dubboprovider/api/impl/ValidateProviderImpl.java
@@ -1,17 +1,18 @@
-//package com.zach.dubboprovider.api.impl;
-//
+package com.zach.dubboprovider.api.impl;
+
 //import com.zach.dubboprovider.api.ValidateProvider;
 //import com.zach.dubboprovider.dto.ValidateParam;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 //public class ValidateProviderImpl implements ValidateProvider {
-//    private static Logger logger = LoggerFactory.getLogger(ValidateProviderImpl.class);
-//
+public class ValidateProviderImpl {
+    private static Logger logger = LoggerFactory.getLogger(ValidateProviderImpl.class);
+
 //    @Override
 //    public boolean paramValidate(ValidateParam validateParam) {
 //        logger.info("hello man !");
 //
 //        return false;
 //    }
-//}
+}
