80dcd4deed53f7641c4cfd790e86bb51b4d30707
maluyong <maluyong@dianwoba.com>
2018-06-28 18:38:26 +0800

    log-000 添加akka 模型.

---
+++ akka/src/main/java/com/zach/akka/App.java
@@ -0,0 +1,13 @@
+package com.zach.akka;
+
+/**
+ * Hello world!
+ */
+public class App {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
