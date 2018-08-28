99bff27b7afccf7a161ac57ed976cf40a9f08952
maluyong <maluyong@dianwoba.com>
2018-08-28 21:51:30 +0800

    log-000 test 代码丢失.

--- util/src/main/java/com/zach/util/page/TestLost.java
+++ util/src/main/java/com/zach/util/page/TestLost.java
@@ -6,6 +6,10 @@ package com.zach.util.page;
  * @create 2018-08-28 20:43
  */
 public class TestLost {
+    public static void dev4(String[] args) {
+
+    }
+
     public static void dev3(String[] args) {
 
     }
