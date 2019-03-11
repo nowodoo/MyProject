78a5904638e385038a88735143354302c89e276f
maluyong <maluyong@dianwoba.com>
2019-03-11 16:59:15 +0800

    log-000 自定义拒绝策略.

--- common/src/main/java/com/zach/common/interview/threadpool/reject/CustomRejectionPolicy.java
+++ common/src/main/java/com/zach/common/interview/threadpool/reject/CustomRejectionPolicy.java
@@ -19,5 +19,9 @@ public class CustomRejectionPolicy implements RejectedExecutionHandler {
     public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
         System.out.println("拒绝策略, 线程信息 ----> " + r.toString());
         //这里可以将任务放在redis或者rocketMQ或者其他的什么地方.来异步处理拒绝的消息. 看业务是不是有对应的需要.
+
+
+        //https://examples.javacodegeeks.com/core-java/util/concurrent/rejectedexecutionhandler/java-util-concurrent-rejectedexecutionhandler-example/
+        //上面的url是一个例子,使用的是另一个线程池进行处理.
     }
 }
