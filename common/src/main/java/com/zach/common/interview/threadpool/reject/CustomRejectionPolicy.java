52a4cfc68ad2cb364110b9ee6a421372e38adf40
maluyong <maluyong@dianwoba.com>
2019-03-11 16:05:24 +0800

    log-000 自定义线程池.

--- common/src/main/java/com/zach/common/interview/threadpool/reject/CustomRejectionPolicy.java
+++ common/src/main/java/com/zach/common/interview/threadpool/reject/CustomRejectionPolicy.java
@@ -11,6 +11,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class CustomRejectionPolicy implements RejectedExecutionHandler {
     @Override
     public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-        System.out.println("拒绝策略 !");
+        System.out.println("拒绝策略, 线程信息 ----> " + r.toString());
     }
 }
