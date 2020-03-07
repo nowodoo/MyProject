28cf37ac4414ceb7b114da66ecd44293a5eb1536
wanqin <luyong.mly@alibaba-inc.com>
2020-03-07 22:03:09 +0800

    时间.

--- common/src/main/java/com/zach/common/time/TimeDemo.java
+++ common/src/main/java/com/zach/common/time/TimeDemo.java
@@ -12,32 +12,60 @@ import java.util.Locale;
 
 public class TimeDemo {
     public static void main(String[] args) {
+        String s = longToStr(1573918840000L);
+        System.out.println(s);
+    }
 
+    public static DateTime strToDateCommon(Date date) {
         //string to Date
         DateTimeFormatter format = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
         DateTime dateTime1 = DateTime.parse("2012-12-21 23:22:45", format);
         System.out.println(dateTime1);
 
+        return dateTime1;
+    }
 
+
+    public static String dateToStr(Date date) {
         //Date to string or some other form
-        Date date = new Date();
         DateTime dateTime2 = new DateTime(date);
         String dateTime2String = dateTime2.toString("yyyy-MM-dd HH:mm:ss");
         System.out.println(dateTime2String);
 
+        return dateTime2String;
+    }
 
+
+    public static DateTime calendarToDate(Date date) {
         //calendar to datetime
         Calendar calendar = Calendar.getInstance();
         DateTime dateTime3 = new DateTime(calendar);
 
+        return dateTime3;
+    }
 
+
+    public static Calendar ToCalendar(Date date) {
         //dateTime to calendar
         DateTime dateTime4 = new DateTime();
-        dateTime4.toCalendar(Locale.CHINA);
+        Calendar calendar = dateTime4.toCalendar(Locale.CHINA);
 
+        return calendar;
+    }
 
+    public static String ToStrJDK(Date date) {
         //Date to string JDK
         SimpleDateFormat formatDateToStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String str = formatDateToStr.format(date);
+
+        return str;
+    }
+
+
+    public static String longToStr(Long l) {
+        Date date = new Date(l);
+        String dateStr = new DateTime(date).toString("yyyy-MM-dd HH:mm:ss");
+
+        return dateStr;
     }
 }
